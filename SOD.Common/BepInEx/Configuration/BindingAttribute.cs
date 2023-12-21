f6b332e92d1b689b34886794ce8611cdd3e4d9e5
stefvandenbruel <stefvandenbruel@outlook.com>
2023-12-21 13:47:03 +0100

    Remove this comment

--- SOD.Common/BepInEx/Configuration/BindingAttribute.cs
+++ SOD.Common/BepInEx/Configuration/BindingAttribute.cs
@@ -18,14 +18,6 @@ namespace SOD.Common.BepInEx.Configuration
         /// </summary>
         public string Description { get; }
 
-        /// <summary>
-        /// The base binding attribute, you can use the Type parameter to specify the type for the defaultValue (for example decimal, float) etc.
-        /// <br>This type parameter is provided because of attribute parameter limitations.</br>
-        /// </summary>
-        /// <param name="defaultValue"></param>
-        /// <param name="description"></param>
-        /// <param name="name"></param>
-        /// <param name="type">The defaultValue's real type</param>
         public BindingAttribute(object defaultValue, string description, string name = null)
         {
             Name = name;
