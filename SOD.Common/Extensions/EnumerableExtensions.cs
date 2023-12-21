6b9aca1b387f54c48b638fd9d051de21d85403e0
Spyraxi (ReveredRedHood) <reveredredhood@pm.me>
2023-12-21 16:23:12 -0700

    Add more comprehensive input detection, add additional extension methods

--- SOD.Common/Extensions/EnumerableExtensions.cs
+++ SOD.Common/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Il2CppSystem.Runtime.InteropServices;
 
 namespace SOD.Common.Extensions
 {
@@ -44,7 +45,7 @@ namespace SOD.Common.Extensions
         }
 
         /// <summary>
-        /// Convert to a regular list
+        /// Convert IL2CPP List to an unmanaged/regular List
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="list"></param>
@@ -52,6 +53,80 @@ namespace SOD.Common.Extensions
         public static List<T> ToList<T>(this Il2CppSystem.Collections.Generic.List<T> list)
         {
             return list.Select(a => a).ToList();
+        }
+
+        /// <summary>
+        /// Convert IL2CPP IList to an unmanaged/regular List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(this Il2CppSystem.Collections.Generic.IList<T> list)
+        {
+            var iListAsList = list.Select(x => x);
+            return iListAsList.ToList();
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(this Il2CppSystem.Collections.Generic.IList<TSource> iList, Func<TSource, TResult> selector)
+        {
+            int index = 0;
+            while (true)
+            {
+                TSource obj;
+                try
+                {
+                    obj = iList[index];
+                    index++;
+                }
+                catch (Exception)
+                {
+                    yield break;
+                }
+                yield return selector.Invoke(obj);
+            }
+        }
+
+        public static IEnumerable<T> Where<T>(this Il2CppSystem.Collections.Generic.IList<T> iList, Func<T, bool> criteria)
+        {
+            int index = 0;
+            while (true)
+            {
+                T obj;
+                try
+                {
+                    obj = iList[index];
+                    index++;
+                }
+                catch (Exception)
+                {
+                    yield break;
+                }
+                if(!criteria(obj)) {
+                    continue;
+                }
+                yield return obj;
+            }
+        }
+
+        /// <summary>
+        /// Allows use of LINQ's Single query while prepending more useful exception information.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <param name="predicate"></param>
+        /// <param name="exceptionMsg">The exception message to be prepended to the original exception.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentException"></exception>
+        public static T Single<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate, string exceptionMsg)
+        {
+            try
+            {
+                return enumerable.Single(predicate);
+            }
+            catch (System.ArgumentException originalException)
+            {
+                throw new System.ArgumentException($"{exceptionMsg} - {originalException.Message}", originalException);
+            }
         }
     }
 }
---
+++ SOD.Common/Patches/InputPatches.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using HarmonyLib;
+using Il2CppInterop.Runtime;
+using Rewired;
+using SOD.Common.Extensions;
+using SOD.Common.Shadows;
+using UnityEngine.Events;
+
+namespace SOD.Common.Patches
+{
+    internal class InputControllerPatches
+    {
+        /// <summary>
+        /// Patch for game input actions which use one of GetButton,
+        /// GetButtonDown, GetButtonUp instead of GetButtonDown and
+        /// GetButtonUp.
+        /// </summary>
+        [HarmonyPatch(typeof(InputController), nameof(InputController.Update))]
+        internal class InputController_Update
+        {
+            // TODO: Also add actionIds, which Rewired docs say makes polling faster.
+            private static List<string> actionNames = new();
+
+            [HarmonyPrefix]
+            internal static void Prefix(InputController __instance)
+            {
+                if (!__instance.enabled || !ReInput.isReady || __instance.player == null)
+                {
+                    return;
+                }
+                if (actionNames.Count == 0)
+                {
+                    var actionList = ReInput.MappingHelper.Instance.Actions.ToList();
+                    foreach (var action in actionList)
+                    {
+                        actionNames.Add(action.name);
+                    }
+                }
+                foreach (var actionName in actionNames)
+                {
+                    if (__instance.player.GetButtonDown(actionName))
+                    {
+                        Lib.InputDetection.ReportButtonStateChange(actionName, true);
+                    }
+                    if (__instance.player.GetButtonUp(actionName))
+                    {
+                        Lib.InputDetection.ReportButtonStateChange(actionName, false);
+                    }
+                }
+            }
+        }
+    }
+}
