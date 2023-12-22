5d421236b3ab040b83ca30103009a30d4ab12afa
venomaus <stefvandenbruel@outlook.com>
2023-12-22 12:09:10 +0100

    Clean up

--- Mods/SOD.StockMarket/Implementation/Market.cs
+++ Mods/SOD.StockMarket/Implementation/Market.cs
@@ -418,8 +418,7 @@ namespace SOD.StockMarket.Implementation
 
             string extension = $".{extType.ToString().ToLower()}";
             var fileName = $"stocks_{savecode}{extension}";
-            var saveStore = Lib.SaveGame.GetSavestoreDirectoryPath(Assembly.GetExecutingAssembly(), fileName);
-            return Path.Combine(saveStore, fileName);
+            return Lib.SaveGame.GetSavestoreDirectoryPath(Assembly.GetExecutingAssembly(), fileName);
         }
     }
 }
