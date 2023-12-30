bb7bffb03d5d6047920a1efbb3402e248fca0d7e
Spyraxi (ReveredRedHood) <reveredredhood@pm.me>
2023-12-30 14:14:54 -0700

    feat: add ShowPlayerSpeech method to GameMessage

--- SOD.Common/Helpers/GameMessage.cs
+++ SOD.Common/Helpers/GameMessage.cs
@@ -1,4 +1,10 @@
-﻿namespace SOD.Common.Helpers
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniverseLib.Utility;
+
+namespace SOD.Common.Helpers
 {
     public sealed class GameMessage
     {
@@ -10,7 +16,14 @@
         /// <param name="icon"></param>
         /// <param name="color"></param>
         /// <param name="delay"></param>
-        public void Broadcast(string message, InterfaceController.GameMessageType messageType = InterfaceController.GameMessageType.notification, InterfaceControls.Icon icon = InterfaceControls.Icon.empty, UnityEngine.Color? color = null, float delay = 0f)
+        public void Broadcast(
+            string message,
+            InterfaceController.GameMessageType messageType =
+                InterfaceController.GameMessageType.notification,
+            InterfaceControls.Icon icon = InterfaceControls.Icon.empty,
+            UnityEngine.Color? color = null,
+            float delay = 0f
+        )
         {
             if (color == null)
                 color = InterfaceControls.Instance.messageRed;
@@ -21,7 +34,63 @@
                 newIcon: icon,
                 colourOverride: true,
                 col: color.Value,
-                newMessageDelay: delay);
+                newMessageDelay: delay
+            );
+        }
+
+        private HashSet<string> queuedPlayerSpeechMessages = new HashSet<string>();
+
+        /// <summary>
+        /// Queues a message on the bottom of the screen resembling the player's internal monologue, like seen in the tutorial during the "wake up" section.
+        /// TODO: Use a less hacky method once we figure out custom DDS strings.
+        /// </summary>
+        /// <param name="message">The message to show.</param>
+        /// <param name="durationSec">How long to show the message for.</param>
+        /// <param name="doNotQueueMultiple">If true, do not queue the message if it is currently being shown or if it is already queued.</param>
+        public void ShowPlayerSpeech(
+            string message,
+            float durationSec,
+            bool doNotQueueMultiple = true
+        )
+        {
+            if (doNotQueueMultiple && queuedPlayerSpeechMessages.Contains(message))
+            {
+                return;
+            }
+            queuedPlayerSpeechMessages.Add(message);
+            Player.Instance.speechController.Speak(
+                "",
+                "temp",
+                false,
+                false,
+                true,
+                0f,
+                true,
+                UnityEngine.Color.white
+            );
+            UniverseLib.RuntimeHelper.StartCoroutine(
+                ShowPlayerSpeechWaitForBubble(message, durationSec)
+            );
+        }
+
+        private IEnumerator ShowPlayerSpeechWaitForBubble(string message, float durationSec)
+        {
+            var speechController = Player.Instance.speechController;
+            while (
+                speechController.activeSpeechBubble.IsNullOrDestroyed(true)
+                || speechController.activeSpeechBubble.actualString != String.Empty
+            )
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            var bubble = speechController.activeSpeechBubble;
+            bubble.actualString = message;
+            bubble.oncreenTime = durationSec;
+            while (!speechController.activeSpeechBubble.IsNullOrDestroyed(true) && speechController.activeSpeechBubble.actualString == message)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            queuedPlayerSpeechMessages.Remove(message);
         }
     }
 }
