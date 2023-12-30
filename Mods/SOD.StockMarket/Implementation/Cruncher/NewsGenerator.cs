3533c068dd95f2e0d4c1ba3d05a4fc205fe5c331
venomaus <stefvandenbruel@outlook.com>
2023-12-31 00:32:48 +0100

    Some boiler plate + ideas

---
+++ Mods/SOD.StockMarket/Implementation/Cruncher/NewsGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOD.StockMarket.Implementation.Cruncher
+{
+    internal static class NewsGenerator
+    {
+        public static List<Article> Articles = new();
+
+        // Some ideas:
+        // Have stocks have other competitor stocks
+        // This means when a news article negatively impacts stock A
+        // Stocks B, C.. might get a positive trend because of this
+
+        // There could also be generic sectors for stocks such as, an article that covers a whole sector (eg industrial)
+        // Then some related industrial stocks could have a trend
+
+        public static Article GenerateArticle()
+        {
+            return null;
+        }
+    }
+
+    internal class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
