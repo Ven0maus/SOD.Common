3ec7eeae26b01f1eb9c9982884e71962943fc7a8
Spyraxi (ReveredRedHood) <reveredredhood@pm.me>
2023-12-30 15:09:08 -0700

    doc: add xml documentation to InteractableInstanceData, add gameObject TryGet method

--- SOD.Common/Custom/InteractableInstanceData.cs
+++ SOD.Common/Custom/InteractableInstanceData.cs
@@ -1,8 +1,12 @@
 using System;
 using UnityEngine;
+using UniverseLib.Utility;
 
 namespace SOD.Common.Custom
 {
+    /// <summary>
+    /// Provides safe access to Interactable data, including TryGet methods for InteractableControllers and Unity objects. Use this to avoid issues when Interactables get pooled.
+    /// </summary>
     public sealed class InteractableInstanceData
     {
         public InteractableInstanceData(Interactable interactable, InteractablePreset preset)
@@ -11,17 +15,33 @@ namespace SOD.Common.Custom
             Preset = preset;
         }
 
+        /// <summary>
+        /// Whether the interactable is loaded. The interactable will be unloaded while it is pooled.
+        /// </summary>
         public bool IsLoaded
         {
             get { return Interactable.loadedGeometry; }
         }
+
+        /// <summary>
+        /// The interactable.
+        /// </summary>
         public Interactable Interactable { get; }
+
+        /// <summary>
+        /// The interactable's preset.
+        /// </summary>
         public InteractablePreset Preset { get; }
 
+        /// <summary>
+        /// Get the interactable controller, if the interactable is loaded.
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <returns>False if the InteractableController component is null or destroyed, true otherwise.</returns>
         public bool TryGetInteractableController(out InteractableController controller)
         {
             controller = null;
-            if (Interactable.controller != null)
+            if (!Interactable.controller.IsNullOrDestroyed(true))
             {
                 controller = Interactable.controller;
                 return true;
@@ -29,6 +49,11 @@ namespace SOD.Common.Custom
             return false;
         }
 
+        /// <summary>
+        /// Get the interactable's transform, if the interactable is loaded.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <returns>False if the GameObject is null or destroyed, true otherwise.</returns>
         public bool TryGetTransform(out Transform transform)
         {
             transform = null;
@@ -37,6 +62,22 @@ namespace SOD.Common.Custom
                 return false;
             }
             transform = controller.transform;
+            return false;
+        }
+
+        /// <summary>
+        /// Get the interactable's GameObject, if the interactable is loaded.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns>False if the GameObject is null or destroyed, true otherwise.</returns>
+        public bool TryGetTransform(out GameObject gameObject)
+        {
+            gameObject = null;
+            if (!TryGetInteractableController(out var controller))
+            {
+                return false;
+            }
+            gameObject = controller.gameObject;
             return false;
         }
 
--- SOD.Common/Extensions/InteractableExtensions.cs
+++ SOD.Common/Extensions/InteractableExtensions.cs
@@ -5,19 +5,34 @@ namespace SOD.Common.Extensions
 {
     public static class InteractableExtensions
     {
-        public static InteractableInstanceData GetValidatedData(this Interactable interactable)
+        /// <summary>
+        /// Get the safe reference to the interactable and related data.
+        /// </summary>
+        /// <param name="interactable"></param>
+        /// <returns>An object that allows safe access to the interactable, its preset, and its controller (if it exists).</returns>
+        public static InteractableInstanceData GetInteractableInstanceData(this Interactable interactable)
         {
             return interactable; // utilize implicit operator logic
         }
 
-        public static InteractableInstanceData GetValidatedData(
+        /// <summary>
+        /// Get the safe reference to the interactable and related data.
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <returns>An object that allows safe access to the interactable, its preset, and its controller.</returns>
+        public static InteractableInstanceData GetInteractableInstanceData(
             this InteractableController controller
         )
         {
             return controller; // utilize implicit operator logic
         }
 
-        public static InteractableInstanceData GetValidatedInteractableData(Transform transform)
+        /// <summary>
+        /// Get the safe reference to the interactable if an InteractableController component exists on this transform or its children, and related data.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <returns>An object that allows safe access to the interactable, its preset, and its controller.</returns>
+        public static InteractableInstanceData GetInteractableInstanceData(Transform transform)
         {
             // TODO: not certain if we should include inactive or not, decided not for now
             var controller = transform.GetComponentInChildren<InteractableController>(
@@ -32,9 +47,14 @@ namespace SOD.Common.Extensions
             return controller; // utilize implicit operator logic
         }
 
-        public static InteractableInstanceData GetValidatedInteractableData(GameObject gameObject)
+        /// <summary>
+        /// Get the safe reference to the interactable if an InteractableController component exists on this GameObject's transform or its children, and related data.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns>An object that allows safe access to the interactable, its preset, and its controller.</returns>
+        public static InteractableInstanceData GetInteractableInstanceData(GameObject gameObject)
         {
-            return GetValidatedInteractableData(gameObject.transform);
+            return GetInteractableInstanceData(gameObject.transform);
         }
     }
 }
