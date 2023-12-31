9c273e0f8fb3283eaaefdff4ecaa4703c0aed70b
venomaus <stefvandenbruel@outlook.com>
2023-12-31 01:10:51 +0100

    Add some news elements to use in articles

--- Mods/SOD.StockMarket/Implementation/Cruncher/News/NewsGenerator.cs
+++ Mods/SOD.StockMarket/Implementation/Cruncher/News/NewsGenerator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SOD.StockMarket.Implementation.Cruncher
+namespace SOD.StockMarket.Implementation.Cruncher.News
 {
     internal static class NewsGenerator
     {
