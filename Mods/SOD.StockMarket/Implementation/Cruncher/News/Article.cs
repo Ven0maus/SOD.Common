4a479d927cc529727286983badbd2095cb30774a
venomaus <stefvandenbruel@outlook.com>
2023-12-31 01:30:13 +0100

    Some serialization stuff for articles

---
+++ Mods/SOD.StockMarket/Implementation/Cruncher/News/Article.cs
@@ -0,0 +1,46 @@
+﻿using SOD.Common.Helpers;
+using System.Text.Json.Serialization;
+
+namespace SOD.StockMarket.Implementation.Cruncher.News
+{
+    internal class Article
+    {
+        [JsonIgnore]
+        private Time.TimeData? _dateTime;
+        [JsonIgnore]
+        public Time.TimeData DateTime
+        {
+            get { return _dateTime ??= new Time.TimeData(Year, Month, Day, Hour, Minute); }
+            set { _dateTime = value; }
+        }
+
+        // Serialization for timedata
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+
+        // Actual properties
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Article() { }
+
+        internal Article(Time.TimeData datetime, string title, string content)
+        {
+            DateTime = datetime;
+
+            // Actual data
+            Title = title;
+            Content = content;
+
+            // Timedata serialization
+            Year = datetime.Year;
+            Month = datetime.Month;
+            Day = datetime.Day;
+            Hour = datetime.Hour;
+            Minute = datetime.Minute;
+        }
+    }
+}
--- Mods/SOD.StockMarket/Implementation/Cruncher/News/NewsGenerator.cs
+++ Mods/SOD.StockMarket/Implementation/Cruncher/News/NewsGenerator.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using SOD.StockMarket.Implementation.Stocks;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SOD.StockMarket.Implementation.Cruncher.News
 {
@@ -18,15 +15,9 @@ namespace SOD.StockMarket.Implementation.Cruncher.News
         // There could also be generic sectors for stocks such as, an article that covers a whole sector (eg industrial)
         // Then some related industrial stocks could have a trend
 
-        public static Article GenerateArticle()
+        public static Article GenerateArticle(Stock stock)
         {
             return null;
         }
-    }
-
-    internal class Article
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
     }
 }
