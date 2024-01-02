e2f734f12ce95001cb7a70e6464009efd2ad0048
venomaus <stefvandenbruel@outlook.com>
2024-01-02 17:33:05 +0100

    Remove logging for next patch

--- SOD.Common/Extensions/TypeExtensions.cs
+++ SOD.Common/Extensions/TypeExtensions.cs
@@ -9,7 +9,6 @@ namespace SOD.Common.Extensions
         public static bool IsAssignableFrom(this Il2CppSystem.Object obj, Type otherType)
         {
             var baseTypes = ReflectionUtility.GetAllBaseTypes(obj);
-            Plugin.Log.LogInfo("All base types: " + string.Join(",", baseTypes.Select(a => a.Name)));
             return baseTypes.Any(a => a == otherType);
         }
     }
