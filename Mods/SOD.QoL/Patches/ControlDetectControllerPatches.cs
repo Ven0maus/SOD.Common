84e2b604a53c9170ab04e194f00aac0f01ea07a5
venomaus <stefvandenbruel@outlook.com>
2024-01-03 22:18:49 +0100

    Skip the "press any key" screen at game start

---
+++ Mods/SOD.QoL/Patches/ControlDetectControllerPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SOD.QoL.Patches
+{
+    internal class ControlDetectControllerPatches
+    {
+        [HarmonyPatch(typeof(ControlDetectController), nameof(ControlDetectController.Start))]
+        internal class ControlDetectController_Start
+        {
+            // Skips the "press any key to start" screen on game start
+
+            [HarmonyPrefix]
+            private static void Prefix(ControlDetectController __instance)
+            {
+                __instance.loadSceneTriggered = true;
+
+                // Set control method to mouse&keyboard or joystick
+                var joystickNames = Input.GetJoystickNames();
+                PlayerPrefs.SetInt("controlMethod", (joystickNames == null || joystickNames.Length == 0) ? 1 : 0);
+            }
+        }
+    }
+}
