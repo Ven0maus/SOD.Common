8315a1dea006e217497dee0b84347513cb3d6cdf
venomaus <stefvandenbruel@outlook.com>
2024-03-10 02:39:32 +0100

    Fix: don't murder too early

--- Mods/SOD.LethalActionReborn/Patches/CitizenPatches.cs
+++ Mods/SOD.LethalActionReborn/Patches/CitizenPatches.cs
@@ -7,14 +7,23 @@ namespace SOD.LethalActionReborn.Patches
         [HarmonyPatch(typeof(Citizen), nameof(Citizen.RecieveDamage))]
         internal static class Citizen_RecieveDamage
         {
-            [HarmonyPostfix]
-            internal static void Postfix(Citizen __instance, float amount, Actor fromWho)
+            private static bool _hasKilled = false;
+
+            [HarmonyPrefix]
+            internal static void Prefix(Citizen __instance, float amount)
             {
-                if (!__instance.isDead && __instance.currentHealth - amount <= 0 && fromWho == Player.Instance)
+                _hasKilled = __instance.currentHealth - amount <= 0;
+            }
+
+            [HarmonyPostfix]
+            internal static void Postfix(Citizen __instance, Actor fromWho)
+            {
+                if (!__instance.isDead && _hasKilled && fromWho == Player.Instance)
                 {
+                    _hasKilled = false;
                     __instance.isStunned = false;
                     __instance.SetHealth(0f);
-                    __instance.ai.SetKO(true, default, default, false, 0f, true, 1f);
+                    //__instance.ai.SetKO(true, default, default, false, 0f, true, 1f);
                     __instance.animationController.cit.Murder(Player.Instance, false, null, null);
                 }
 
