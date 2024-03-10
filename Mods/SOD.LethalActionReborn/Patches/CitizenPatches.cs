9315117d197a739b0fc401f4ab997fa26eacb33d
venomaus <stefvandenbruel@outlook.com>
2024-03-10 01:12:42 +0100

    No dds content

---
+++ Mods/SOD.LethalActionReborn/Patches/CitizenPatches.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+
+namespace SOD.LethalActionReborn.Patches
+{
+    internal class CitizenPatches
+    {
+        [HarmonyPatch(typeof(Citizen), nameof(Citizen.RecieveDamage))]
+        internal static class Citizen_RecieveDamage
+        {
+            [HarmonyPrefix]
+            internal static void Prefix(Citizen __instance, float amount, Actor fromWho, ref bool enableKill)
+            {
+                if (!__instance.isDead && __instance.currentHealth - amount <= 0 && fromWho == Player.Instance)
+                {
+                    __instance.isStunned = false;
+                    if (!enableKill)
+                        enableKill = true;
+                }
+            }
+
+            [HarmonyPostfix]
+            internal static void Postfix(Citizen __instance, Actor fromWho)
+            {
+                if (__instance.isDead && __instance.currentHealth <= 0 && fromWho == Player.Instance)
+                {
+                    if (MurderController.Instance.currentMurderer == __instance)
+                    {
+                        MurderController.Instance.PickNewMurderer();
+                        MurderController.Instance.PickNewVictim();
+                    }
+                    else if (MurderController.Instance.currentVictim == __instance)
+                    {
+                        MurderController.Instance.PickNewVictim();
+                    }
+                }
+            }
+        }
+    }
+}
