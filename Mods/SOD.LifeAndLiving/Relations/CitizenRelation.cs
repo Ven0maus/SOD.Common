4f2ce19ac73199e2dac457993f0707889201b1e0
venomaus <stefvandenbruel@outlook.com>
2024-04-12 16:09:41 +0200

    Add some basic relational information

---
+++ Mods/SOD.LifeAndLiving/Relations/CitizenRelation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SOD.LifeAndLiving.Relations
+{
+    /// <summary>
+    /// Contains information about the relation between the player and the citizen.
+    /// </summary>
+    public sealed class CitizenRelation
+    {
+        /// <summary>
+        /// How many times the player has interacted with this citizen.
+        /// </summary>
+        public int Interacted { get; set; }
+        /// <summary>
+        /// The last time the player interacted with this citizen.
+        /// </summary>
+        public DateTime? LastInteraction { get; set; }
+        /// <summary>
+        /// How many times the player has seen this citizen.
+        /// </summary>
+        public int Seen { get; set; }
+        /// <summary>
+        /// The last time the player has seen this citizen.
+        /// </summary>
+        public DateTime? LastSeen { get; set; }
+        /// <summary>
+        /// The relation between the player and the citizen.
+        /// </summary>
+        public Relation Relation { get; set; }
+    }
+
+    /// <summary>
+    /// All possible relations between the player and the citizens
+    /// </summary>
+    public enum Relation
+    {
+        /// <summary>
+        /// Player is unaware of the citizen, never interacted
+        /// </summary>
+        Stranger,
+        /// <summary>
+        /// Player is aware of the citizen, never interacted
+        /// </summary>
+        Neutral,
+        /// <summary>
+        /// Player has only briefly met them and interacted
+        /// </summary>
+        Acquaintance,
+        /// <summary>
+        /// Player and the citizen are friends
+        /// </summary>
+        Friend,
+        /// <summary>
+        /// Player and the citizen are in dislike of each other
+        /// </summary>
+        Antagonistic,
+        /// <summary>
+        /// Player and the citizen are hostile towards each other
+        /// </summary>
+        Hostile
+    }
+}
---
+++ Mods/SOD.LifeAndLiving/Relations/RelationManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOD.LifeAndLiving.Relations
+{
+    /// <summary>
+    /// Contains methods to create, update and delete relations between citizens and load/save the relation state per savegame.
+    /// </summary>
+    internal sealed class RelationManager
+    {
+        private static RelationManager _instance;
+        /// <summary>
+        /// The instance accessor for the RelationManager class
+        /// </summary>
+        internal static RelationManager Instance => _instance ??= new RelationManager();
+
+        /// <summary>
+        /// A relation matrix dictionary for each unique citizen.
+        /// </summary>
+        private readonly Dictionary<int, CitizenRelation> _relationMatrixes = new();
+
+        private RelationManager() 
+        { }
+
+        /// <summary>
+        /// Updates relation information of a citizen.
+        /// </summary>
+        /// <param name="citizenId"></param>
+        /// <param name="info"></param>
+        public void Set(int citizenId, Action<CitizenRelation> info)
+        {
+            if (!_relationMatrixes.TryGetValue(citizenId, out var relationMatrix))
+                _relationMatrixes[citizenId] = relationMatrix = new CitizenRelation();
+            info.Invoke(relationMatrix);
+        }
+
+        /// <summary>
+        /// Delete's relation information of a citizen.
+        /// </summary>
+        /// <param name="citizenId"></param>
+        public void Delete(int citizenId)
+        {
+            _relationMatrixes.Remove(citizenId);
+        }
+
+        /// <summary>
+        /// Loads all citizen relation information from a save file.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void Load(string filePath)
+        {
+
+        }
+
+        /// <summary>
+        /// Saves all citizen relation information to a save file.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void Save(string filePath)
+        {
+
+        }
+    }
+}
