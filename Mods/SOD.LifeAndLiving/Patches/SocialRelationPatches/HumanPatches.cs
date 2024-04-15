09fe5c0266b66e4161e0bc01146faf2954047155
venomaus <stefvandenbruel@outlook.com>
2024-04-15 23:42:05 +0200

    Improve the seen check

--- Mods/SOD.LifeAndLiving/Patches/SocialRelationPatches/HumanPatches.cs
+++ Mods/SOD.LifeAndLiving/Patches/SocialRelationPatches/HumanPatches.cs
@@ -2,6 +2,7 @@
 using SOD.LifeAndLiving.Relations;
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace SOD.LifeAndLiving.Patches.SocialRelationPatches
 {
@@ -17,19 +18,24 @@ namespace SOD.LifeAndLiving.Patches.SocialRelationPatches
             {
                 // This method is called on a citizen when he sees another citizen, in this case
                 // We are only interested when they see the player
-                if (!citizen.isPlayer) return;
+                if (citizen == null || __instance.isMachine || !citizen.isPlayer) return;
 
-                if (!_lastSeenTimings.TryGetValue(__instance.humanID, out var time))
+                float distance = Vector3.Distance(__instance.lookAtThisTransform.position, citizen.lookAtThisTransform.position);
+                if (distance < GameplayControls.Instance.minimumStealthDetectionRange ||
+                    __instance.ActorRaycastCheck(citizen, distance + 3f, out _, false, Color.green, Color.red, Color.white, 1f))
                 {
-                    _lastSeenTimings.Add(__instance.humanID, time = DateTime.Now.AddMinutes(-2));
-                }
-                
-                if (time.AddMinutes(1) < DateTime.Now)
-                {
-                    _lastSeenTimings[__instance.humanID] = DateTime.Now;
-                    var relation = RelationManager.Instance[__instance.humanID];
-                    relation.LastSeen = time;
-                    relation.Seen++;
+                    if (!_lastSeenTimings.TryGetValue(__instance.humanID, out var time))
+                    {
+                        _lastSeenTimings.Add(__instance.humanID, time = DateTime.Now.AddMinutes(-2));
+                    }
+
+                    if (time.AddMinutes(1) < DateTime.Now)
+                    {
+                        _lastSeenTimings[__instance.humanID] = DateTime.Now;
+                        var relation = RelationManager.Instance[__instance.humanID];
+                        relation.LastSeen = time;
+                        relation.Seen++;
+                    }
                 }
             }
         }
