878ca46f768e94c6b9dcc347dd0a368d333ad0ef
venomaus <stefvandenbruel@outlook.com>
2024-04-19 17:55:33 +0200

    Changes for the dialog

--- Mods/SOD.LifeAndLiving/Relations/Dialogs/TheUsualDialog.cs
+++ Mods/SOD.LifeAndLiving/Relations/Dialogs/TheUsualDialog.cs
@@ -1,4 +1,5 @@
-﻿using SOD.Common;
+﻿using Rewired;
+using SOD.Common;
 using SOD.Common.Helpers.DialogObjects;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,15 @@ namespace SOD.LifeAndLiving.Relations.Dialogs
 {
     internal class TheUsualDialog : IDialogLogic
     {
+        private static readonly Guid _positiveResponse = Guid.NewGuid();
+        private static readonly Guid _positiveDiscountResponse = Guid.NewGuid();
+        private static readonly Guid _positiveFreeResponse = Guid.NewGuid();
+
+        // TODO: Move to configuration bindings
+        private const int DiscountPercentage = 35;
+        private const int DiscountChance = 25;
+        private const int FreeChance = 10;
+
         /// <summary>
         /// Add's several dialog options regarding purchasing items often at places.
         /// </summary>
@@ -15,12 +25,14 @@ namespace SOD.LifeAndLiving.Relations.Dialogs
         {
             _ = Lib.Dialog.Builder($"{Plugin.PLUGIN_GUID.GetHashCode()}_TheUsualPurchase")
                 .SetText("The usual please.")
-                .AddResponse("Ahh yes, coming right up sir!", isSuccesful: true)
+                .AddCustomResponse("Ahh yes, coming right up sir!", _positiveResponse)
+                .AddCustomResponse("You're lucky, this last one is 50% off the price!", _positiveDiscountResponse)
+                .AddCustomResponse("Coming right up, this one's on the house!", _positiveFreeResponse)
                 .AddResponse("Looks like you can't afford it today.", isSuccesful: false)
                 .ModifyDialogOptions((a) =>
                 {
                     a.useSuccessTest = true;
-                    a.ranking = 100;
+                    a.ranking = int.MaxValue;
                 })
                 .SetDialogLogic(new TheUsualDialog())
                 .CreateAndRegister();
@@ -32,6 +44,7 @@ namespace SOD.LifeAndLiving.Relations.Dialogs
         {
             // Check if this citizen has sold items to us before, and if one of them was sold more or equal than 5 times
             if (saysTo == null || !saysTo.isAtWork) return false;
+            // TODO: Check also if the player has a free inventory slot!
             if (!RelationManager.Instance.PlayerInterests.PurchasedItemsFrom.TryGetValue(saysTo.humanID, out var items))
                 return false;
             return items.Any(a => a.Value >= 5);
@@ -40,13 +53,24 @@ namespace SOD.LifeAndLiving.Relations.Dialogs
         public void OnDialogExecute(DialogController instance, Citizen saysTo, Interactable saysToInteractable, NewNode where, Actor saidBy, bool success, NewRoom roomRef, SideJob jobRef)
         {
             // Collect most purchased item
-            if (_item != null)
+            if (success && _item != null)
             {
-                // TODO: Give item to the player, reduce money if required
+                // TODO: Give item to the player
 
-                Plugin.Log.LogInfo("Received item");
+                // Pay for the item if its not free, and provide a custom response
+                if (_item.Price > 0)
+                {
+                    GameplayController.Instance.AddMoney(-_item.Price, false, "Purchase");
+                    saysTo.speechController.Speak(_item.Discount ? _positiveDiscountResponse.ToString() : _positiveResponse.ToString());
+                }
+                else if (_item.Price == 0)
+                {
+                    saysTo.speechController.Speak(_positiveFreeResponse.ToString());
+                }
 
-                // Reset to null
+                Plugin.Log.LogInfo("Received item: " + _item.Name);
+
+                // Reset to null for next dialogue
                 _item = null;
             }
         }
@@ -63,11 +87,23 @@ namespace SOD.LifeAndLiving.Relations.Dialogs
                 return DialogController.ForceSuccess.fail;
             }
 
-            // TODO: Check if we have enough cash
-            // Or succes if its on the house
+            // Check for discounts
+            var rand = Toolbox.Instance.Rand(0, 100, true);
+            if (rand < FreeChance)
+                _item.Price = 0;
+            else if (rand < DiscountChance)
+            {
+                _item.Price -= (int)Math.Round(_item.Price / 100 * (float)DiscountPercentage);
+                _item.Discount = true;
+            }
 
-            //if (notEnoughCash)
-            //  return DialogController.ForceSuccess.fail;
+            // Make sure we don't go below 0
+            if (_item.Price < 0)
+                _item.Price = 0;
+
+            // If we don't have enough money its a fail!
+            if (GameplayController.Instance.money < _item.Price)
+                return DialogController.ForceSuccess.fail;
 
             return DialogController.ForceSuccess.none;
         }
@@ -108,7 +144,8 @@ namespace SOD.LifeAndLiving.Relations.Dialogs
         {
             public string Name { get; }
             public Interactable Interactable { get; }
-            public int Price { get; }
+            public int Price { get; set; }
+            public bool Discount { get; set; } = false;
 
             public Item(string itemName, Interactable interactable, int price)
             {
