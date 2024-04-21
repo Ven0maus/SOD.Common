22b457415d7fa5013ab29ef350a8c2de61f65f0d
venomaus <stefvandenbruel@outlook.com>
2024-04-21 19:34:24 +0200

    Simplified approach

--- SOD.Common/Helpers/PlayerStatus.cs
+++ SOD.Common/Helpers/PlayerStatus.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using SOD.Common.Custom;
-using SOD.Common.Extensions;
-using UnityEngine;
-using UniverseLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SOD.Common.Helpers
 {
@@ -21,50 +18,50 @@ namespace SOD.Common.Helpers
         /// </summary>
         public event EventHandler<IllegalActionStatusArgs> OnAfterSetIllegalStatus;
 
-        internal void OnSetIllegalActionStatus(IllegalActionStatusArgs eventArgs, bool after)
-        {
-            if (after)
-                OnAfterSetIllegalStatus?.Invoke(this, eventArgs);
-            else
-                OnBeforeSetIllegalStatus?.Invoke(this, eventArgs);
-        }
-
-        internal Dictionary<string, IllegalStatusModifier> IllegalStatusModifierDictionary = new Dictionary<string, IllegalStatusModifier>();
-
-        internal Coroutine UpdateIllegalStatusModifiersCoroutine { get; set; }
+        /// <summary>
+        /// Dictionary used to track illegal status for the player entity.
+        /// </summary>
+        internal Dictionary<string, IllegalStatusModifier> IllegalStatusModifierDictionary { get; private set; }
 
         /// <summary>
-        /// Sets an illegal status modifier for the specified duration, or
-        /// indefinitely if the duration is 0.0f or omitted. The modifier will
-        /// cause the Player to have an Illegal Action status while it or any
-        /// other modifier is present. At the end of the duration (if
-        /// provided), the modifier will be automatically removed.
+        /// Sets an illegal status modifier with an optional duration.
+        /// <br>The modifier will cause the Player to have an Illegal Action status while it or any other modifier is present.</br>
+        /// <br>At the end of the duration (if provided), the modifier will be automatically removed.</br>
         /// </summary>
         /// <param name="key">The key identifying the modifier. Used to overwrite, remove, or toggle the modifier later.</param>
-        /// <param name="force">Whether to overwrite any existing modifier with this key, applying the new duration.</param>
-        /// <param name="duration">The duration that the modifier lasts, in seconds (does not progress while the game is paused).</param>
-        public void SetIllegalStatusModifier(string key, bool force = false, float duration = 0.0f)
+        /// <param name="overwrite">Whether to overwrite any existing modifier with this key, applying the new duration.</param>
+        /// <param name="duration">The duration that the modifier lasts. (does not progress while the game is paused).</param>
+        public void SetIllegalStatusModifier(string key, bool overwrite = false, TimeSpan? duration = null)
         {
-            if (key == "")
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentException("Key cannot be null or empty.", nameof(key));
+
+            if (IllegalStatusModifierDictionary == null)
+                IllegalStatusModifierDictionary = new();
+
+            if (IllegalStatusModifierDictionary.TryGetValue(key, out var modifier))
             {
-                throw new InvalidOperationException("Empty keys are prohibited.");
+                if (overwrite)
+                {
+                    modifier.Stop();
+                    IllegalStatusModifierDictionary.Remove(key);
+                }
+                else
+                {
+                    return;
+                }
             }
-            if (force && IllegalStatusModifierDictionary.ContainsKey(key))
-            {
-                // Remove entry with the same key, if it exists. Do not stop
-                // the coroutine if it is started.
-                IllegalStatusModifierDictionary.Remove(key);
-            }
-            else if (!force)
-            {
-                // Modifier with this key is already present
-                return;
-            }
-            IllegalStatusModifierDictionary.Add(key, new IllegalStatusModifier(key, duration, duration == 0.0f ? false : true));
-            if (UpdateIllegalStatusModifiersCoroutine == null)
-            {
-                UpdateIllegalStatusModifiersCoroutine = RuntimeHelper.StartCoroutine(UpdateIllegalStatusModifiers());
-            }
+
+            // Add modifier to dictionary to track
+            modifier = new IllegalStatusModifier(key, duration);
+            IllegalStatusModifierDictionary.Add(key, modifier);
+
+            // Start timer
+            if (duration != null)
+                modifier.Start();
+
+            // Update status
+            Update();
         }
 
         /// <summary>
@@ -73,109 +70,64 @@ namespace SOD.Common.Helpers
         /// <param name="key">The key identifying the modifier.</param>
         public void RemoveIllegalStatusModifier(string key)
         {
-            if (key == "")
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentException("Key cannot be null or empty.", nameof(key));
+
+            if (IllegalStatusModifierDictionary != null && IllegalStatusModifierDictionary.TryGetValue(key, out var modifier))
             {
-                // No need to throw
-                return;
-            }
-            if (IllegalStatusModifierDictionary.ContainsKey(key))
-            {
+                modifier.Stop();
                 IllegalStatusModifierDictionary.Remove(key);
-            }
-            if (IllegalStatusModifierDictionary.Count > 0)
-            {
-                return;
-            }
-            if (UpdateIllegalStatusModifiersCoroutine != null)
-            {
-                RuntimeHelper.StopCoroutine(UpdateIllegalStatusModifiersCoroutine);
-                UpdateIllegalStatusModifiersCoroutine = null;
+
+                // Update status
+                Update();
             }
         }
 
-        /// <summary>
-        /// Toggles an illegal status modifier by adding it if the key is not present, or removing it if the key is present. If added, the modifier will not have a duration associated with it (it will be active indefinitely, until toggled or removed).
-        /// </summary>
-        /// <param name="key">The key identifying the modifier.</param>
-        public void ToggleIllegalStatusModifier(string key)
+        internal void Update()
         {
-            if (IllegalStatusModifierDictionary.ContainsKey(key))
-            {
-                RemoveIllegalStatusModifier(key);
-                return;
-            }
-            SetIllegalStatusModifier(key);
+            if (IllegalStatusModifierDictionary.Any())
+                AdjustIllegalStatus(Player.Instance.illegalStatus, true);
+            else
+                AdjustIllegalStatus(Player.Instance.illegalStatus, false);
         }
 
-        internal IEnumerator UpdateIllegalStatusModifiers()
+        private static void AdjustIllegalStatus(bool previousStatus, bool newStatus)
         {
-            var setAsIllegal = false;
-            var modifierKeysToRemove = new List<string>();
-            var deltaTime = 0.0f;
-            while (true)
+            if (previousStatus != newStatus)
             {
-                yield return new WaitForEndOfFrame();
+                var args = new IllegalActionStatusArgs(previousStatus, newStatus);
 
-                if (Lib.Time.IsGamePaused)
-                {
-                    continue;
-                }
-
-                setAsIllegal = false;
-                deltaTime = UnityEngine.Time.deltaTime;
-
-                foreach (var key in IllegalStatusModifierDictionary.Keys)
-                {
-                    var entry = IllegalStatusModifierDictionary[key];
-                    if (!entry.UseTime)
-                    {
-                        setAsIllegal = true;
-                        continue;
-                    }
-                    entry.TimeRemainingSec -= deltaTime;
-                    if (entry.TimeRemainingSec > 0.0f)
-                    {
-                        setAsIllegal = true;
-                        continue;
-                    }
-                    // Expired timer, so ignore it and queue the modifier to be
-                    // removed
-                    modifierKeysToRemove.Add(entry.Key);
-                }
-
-                modifierKeysToRemove.ForEach(key => IllegalStatusModifierDictionary.Remove(key));
-                modifierKeysToRemove.Clear();
-
-                if (Player.Instance.illegalStatus == setAsIllegal)
-                {
-                    // Avoid constant changing back and forth
-                    Player.Instance.illegalActionTimer = deltaTime * 2.0f;
-                    continue;
-                }
-
-                // We want our illegal status to fall off as soon as the
-                // dictionary is empty, but to last until at least the next
-                // frame.
-                // Use deltaTime * 2.0f to prevent issues where the timer
-                // immediately expires during the UpdateIllegalStatus call
-                var args = new IllegalActionStatusArgs(setAsIllegal);
+                // Raise before event
                 Lib.PlayerStatus.OnSetIllegalActionStatus(args, false);
-                Player.Instance.illegalActionActive = setAsIllegal;
-                Player.Instance.illegalActionTimer = deltaTime * 2.0f;
+
+                // Adjust illegal status
+                Player.Instance.illegalStatus = newStatus;
+                Player.Instance.illegalActionTimer = newStatus ? float.MaxValue : 0f;
                 Player.Instance.UpdateIllegalStatus();
-                Lib.PlayerStatus.OnSetIllegalActionStatus(args, true);
+
+                // Raise after event
+                Lib.PlayerStatus.OnSetIllegalActionStatus(args, newStatus);
             }
         }
 
         public sealed class IllegalActionStatusArgs : EventArgs
         {
-            public bool SetAsIllegal { get; }
+            public bool PreviousIllegalStatus { get; }
+            public bool NewIllegalStatus { get; }
 
-            internal IllegalActionStatusArgs(bool setAsIllegal)
+            internal IllegalActionStatusArgs(bool previous, bool @new)
             {
-                SetAsIllegal = setAsIllegal;
+                PreviousIllegalStatus = previous;
+                NewIllegalStatus = @new;
             }
         }
-    }
 
+        internal void OnSetIllegalActionStatus(IllegalActionStatusArgs eventArgs, bool after)
+        {
+            if (after)
+                OnAfterSetIllegalStatus?.Invoke(this, eventArgs);
+            else
+                OnBeforeSetIllegalStatus?.Invoke(this, eventArgs);
+        }
+    }
 }
