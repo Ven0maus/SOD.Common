20d7c6ed67fdc5df5d2e5ac5a7b7474ff7832c08
venomaus <stefvandenbruel@outlook.com>
2024-04-26 16:57:13 +0200

    Seperate know and life config modifiers to be more clear

--- Mods/SOD.RelationsPlus/IPluginBindings.cs
+++ Mods/SOD.RelationsPlus/IPluginBindings.cs
@@ -2,24 +2,27 @@
 
 namespace SOD.RelationsPlus
 {
-    public interface IPluginBindings : IModifierBindings
+    public interface IPluginBindings : IKnowModifierBindings, ILikeModifierBindings
     { }
 
-    public interface IModifierBindings
+    public interface IKnowModifierBindings
     {
-        [Binding(0.015f, "How much the \"Know\" property changes for the citizen and player when seen passing by on the street.", "Modifiers.SeenOnStreetModifier")]
+        [Binding(0.015f, "How much the \"Know\" property changes for the citizen and player when seen passing by on the street.", "Modifiers.Know.SeenOnStreetModifier")]
         float SeenOnStreetModifier { get; set; }
 
-        [Binding(0.025f, "How much the \"Know\" property changes for the citizen and player when seen at their workplace.", "Modifiers.SeenAtWorkplaceModifier")]
+        [Binding(0.025f, "How much the \"Know\" property changes for the citizen and player when seen at their workplace.", "Modifiers.Know.SeenAtWorkplaceModifier")]
         float SeenAtWorkplaceModifier { get; set; }
 
-        [Binding(0.045f, "How much the \"Know\" property changes for the citizen and player when seen inside their home.", "Modifiers.SeenInHome")]
+        [Binding(0.045f, "How much the \"Know\" property changes for the citizen and player when seen inside their home.", "Modifiers.Know.SeenInHome")]
         float SeenInHomeModifier { get; set; }
 
-        [Binding(0.035f, "How much the \"Know\" property changes for the citizen and player when seen inside their home's building/apartement.", "Modifiers.SeenInHomeBuilding")]
+        [Binding(0.035f, "How much the \"Know\" property changes for the citizen and player when seen inside their home's building/apartement.", "Modifiers.Know.SeenInHomeBuilding")]
         float SeenInHomeBuildingModifier { get; set; }
+    }
 
-        [Binding(-0.05f, "How much the \"Like\" property changes for the citizen and player when seen trespassing.", "Modifiers.SeenTrespassingModifier")]
+    public interface ILikeModifierBindings
+    {
+        [Binding(-0.05f, "How much the \"Like\" property changes for the citizen and player when seen trespassing.", "Modifiers.Like.SeenTrespassingModifier")]
         float SeenTrespassingModifier { get; set; }
     }
 }
