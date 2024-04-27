5edbe894603a92e7a0919c12f1588739c87196dd
venomaus <stefvandenbruel@outlook.com>
2024-04-28 00:50:06 +0200

    Cleanup warnings that are not useful

--- SOD.Common/Helpers/DialogObjects/BlockCondition.cs
+++ SOD.Common/Helpers/DialogObjects/BlockCondition.cs
@@ -5,7 +5,9 @@
         internal BlockCondition() { }
 
         // Hide some properties exposed from the game
+#pragma warning disable CS0649
         internal new string blockID;
         internal new string instanceID;
+#pragma warning restore CS0649
     }
 }
