3ebe8a69761a0592fb100c3781167e62dbf7d4c4
venomaus <stefvandenbruel@outlook.com>
2024-05-02 01:37:11 +0200

    Better alternative

--- Mods/SOD.RelationsPlus/Patches/InteractionControllerPatches.cs
+++ Mods/SOD.RelationsPlus/Patches/InteractionControllerPatches.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System;
+using System.Linq;
 using UnityEngine;
 
 namespace SOD.RelationsPlus.Patches
@@ -16,23 +18,28 @@ namespace SOD.RelationsPlus.Patches
                 // Needs to run everytime, even if the value didn't change
                 foreach (var actor in CityData.Instance.visibleActors)
                 {
-                    var human = actor as Human;
-                    if (human == null || actor.isPlayer ||
-                        !actor.isSeenByOthers || actor.isDead || actor.isStunned || actor.isAsleep)
-                        continue;
-
-                    float distance = Vector3.Distance(human.lookAtThisTransform.position, Player.Instance.lookAtThisTransform.position);
-                    float maxDistance = Mathf.Min(GameplayControls.Instance.citizenSightRange, human.stealthDistance);
-                    if (distance <= maxDistance)
+                    try
                     {
-                        if (distance < GameplayControls.Instance.minimumStealthDetectionRange ||
-                            human.ActorRaycastCheck(Player.Instance, distance + 3f, out _, false, Color.green, Color.red, Color.white, 1f))
+                        var human = ((dynamic)actor).Cast<Human>();
+                        if (human == null || actor.isPlayer ||
+                            !actor.isSeenByOthers || actor.isDead || actor.isStunned || actor.isAsleep)
+                            continue;
+
+                        float distance = Vector3.Distance(human.lookAtThisTransform.position, Player.Instance.lookAtThisTransform.position);
+                        float maxDistance = Mathf.Min(GameplayControls.Instance.citizenSightRange, human.stealthDistance);
+                        if (distance <= maxDistance)
                         {
-                            if (Plugin.Instance.Config.DebugMode)
-                                Plugin.Log.LogInfo($"Illegal activity seen by {human.GetCitizenName()}!");
-                            RelationManager.Instance[human.humanID].Like += Plugin.Instance.Config.SeenDoingIllegalModifier;
+                            if (distance < GameplayControls.Instance.minimumStealthDetectionRange ||
+                                human.ActorRaycastCheck(Player.Instance, distance + 3f, out RaycastHit _, false, Color.green, Color.red, Color.white, 1f))
+                            {
+                                if (Plugin.Instance.Config.DebugMode)
+                                    Plugin.Log.LogInfo($"Illegal activity seen by {human.GetCitizenName()}!");
+                                RelationManager.Instance[human.humanID].Like += Plugin.Instance.Config.SeenDoingIllegalModifier;
+                            }
                         }
                     }
+                    catch(InvalidCastException)
+                    { }
                 }
             }
         }
