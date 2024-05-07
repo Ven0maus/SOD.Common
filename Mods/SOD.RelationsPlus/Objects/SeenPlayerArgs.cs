1e361165b3e39477ac1b78451a330f5ff76d4e26
venomaus <stefvandenbruel@outlook.com>
2024-05-07 11:22:37 +0200

    Revert "Revert "Merge branch 'feature/relationsplus' into develop""
    This reverts commit 63c441585b50157ea1c42077c66e925d203e0b73.
    
    # Conflicts:
    #	SOD.Common/Extensions/GameExtensions.cs

---
+++ Mods/SOD.RelationsPlus/Objects/SeenPlayerArgs.cs
@@ -0,0 +1,53 @@
+﻿using SOD.Common;
+using SOD.Common.Helpers;
+using System;
+
+namespace SOD.RelationsPlus.Objects
+{
+    public sealed class SeenPlayerArgs : EventArgs
+    {
+        /// <summary>
+        /// The id of the citizen.
+        /// </summary>
+        public int CitizenId { get; }
+        /// <summary>
+        /// The value that "Know" was changed by.
+        /// </summary>
+        public float KnowChange { get; }
+        /// <summary>
+        /// The value that "Like" was changed by.
+        /// <br>It can be changed in the event of tresspassing.</br>
+        /// </summary>
+        public float LikeChange { get; }
+        /// <summary>
+        /// Where the player was seen by the citizen.
+        /// </summary>
+        public SeenLocation Location { get; }
+        /// <summary>
+        /// The time the player was seen by this citizen in real time.
+        /// </summary>
+        public DateTime SeenRealTime { get; }
+        /// <summary>
+        /// The time the player was seen by this citizen in game time.
+        /// </summary>
+        public Time.TimeData SeenGameTime { get; }
+
+        internal SeenPlayerArgs(int citizenId, SeenLocation location, float knowChange, float likeChange)
+        {
+            CitizenId = citizenId;
+            KnowChange = knowChange;
+            LikeChange = likeChange;
+            Location = location;
+            SeenRealTime = DateTime.Now;
+            SeenGameTime = Lib.Time.CurrentDateTime;
+        }
+
+        public enum SeenLocation
+        {
+            Street,
+            Workplace,
+            Home,
+            HomeBuilding
+        }
+    }
+}
---
+++ Mods/SOD.RelationsPlus/Patches/InteractionControllerPatches.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using System;
+using SOD.Common.Extensions;
+
+namespace SOD.RelationsPlus.Patches
+{
+    internal class InteractionControllerPatches
+    {
+        [HarmonyPatch(typeof(InteractionController), nameof(InteractionController.SetIllegalActionActive))]
+        internal static class InteractionController_SetIllegalActionActive
+        {
+            [HarmonyPostfix]
+            internal static void Prefix(bool val)
+            {
+                if (!val) return;
+
+                // Needs to run everytime, even if the value didn't change
+                foreach (var actor in CityData.Instance.visibleActors)
+                {
+                    if (actor.isMachine || actor.isPlayer) continue;
+                    if (!actor.Sees(Player.Instance)) continue;
+
+                    Human human = null;
+                    try
+                    {
+                        human = ((dynamic)actor).Cast<Human>();
+                    }
+                    catch(InvalidCastException)
+                    { }
+
+                    if (human == null) continue;
+
+                    if (Plugin.Instance.Config.DebugMode)
+                        Plugin.Log.LogInfo($"[Debug]: Illegal activity seen by {human.GetCitizenName()}!");
+
+                    RelationManager.Instance[human.humanID].Like += Plugin.Instance.Config.SeenDoingIllegalModifier;
+                }
+            }
+        }
+    }
+}
