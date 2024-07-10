6b1838d17aeefdd7402beed5df92695f7d568f32
venomaus <stefvandenbruel@outlook.com>
2024-07-11 00:43:01 +0200

    Bugfix: AirVent didn't require a screwdriver, now it does

---
+++ Mods/SOD.LifeAndLiving/Bindings/ImRequirementBindings.cs
@@ -0,0 +1,10 @@
+﻿using SOD.Common.BepInEx.Configuration;
+
+namespace SOD.LifeAndLiving.Bindings.ImRequirementBindings
+{
+    public interface ImRequirementBindings
+    {
+        [Binding(true, "Should the player need a screwdriver in their inventory to open vents?", "LifeAndLiving.ImmersiveRequirements.RequireScrewdriverForVents")]
+        bool RequireScrewdriverForVents { get; set; }
+    }
+}
