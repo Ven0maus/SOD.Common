ff66fe00f9647efaf8ad36f651b3fa2808d2a8d4
venomaus <stefvandenbruel@outlook.com>
2024-08-19 20:09:30 +0200

    Add improvements + detective related helpers

---
+++ SOD.Common/Helpers/DetectiveObjects/VictimArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SOD.Common.Helpers.DetectiveObjects
+{
+    public sealed class VictimArgs : EventArgs
+    {
+        public Human Victim { get; }
+        public Human Reporter { get; }
+        public Human.Death.ReportType ReportType { get; }
+
+        internal VictimArgs(Human victim, Human reporter, Human.Death.ReportType reportType)
+        {
+            Victim = victim;
+            Reporter = reporter;
+            ReportType = reportType;
+        }
+    }
+}
