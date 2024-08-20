d145f0bb028d8a6dc846103050b18ffbac03cff8
venomaus <stefvandenbruel@outlook.com>
2024-08-20 22:09:21 +0200

    Extra additions to gameplay lib

---
+++ SOD.Common/Helpers/GameplayObjects/InteractableArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SOD.Common.Helpers.GameplayObjects
+{
+    public sealed class InteractableArgs : EventArgs
+    {
+        public Interactable Interactable { get; }
+
+        internal InteractableArgs(Interactable interactable) 
+        { 
+            Interactable = interactable;
+        }
+    }
+}
